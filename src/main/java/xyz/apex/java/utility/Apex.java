7c09934d61f6134decd549ccd8c89acbb4dfc77b
ApexModder <robsonlawson3@gmail.com>
2021-12-01 04:26:11 +0000

    Add methods to make immutable collection types mutable

--- src/main/java/xyz/apex/java/utility/Apex.java
+++ src/main/java/xyz/apex/java/utility/Apex.java
@@ -1,8 +1,6 @@
 package xyz.apex.java.utility;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.*;
 
 import xyz.apex.java.utility.nullness.NonnullUnaryOperator;
 
@@ -41,6 +39,21 @@ public final class Apex
 	}
 
 	/**
+	 * Converts specified {@link List} into a Mutable list.
+	 *
+	 * @param list {@link List} to be made mutable
+	 * @param <V> Object type of values inside the {@link List}
+	 * @return The newly built list
+	 * @since 1.0.4-J8
+	 */
+	public static <V> List<V> makeMutableList(List<V> list)
+	{
+		if(list instanceof ImmutableList)
+			return Lists.newArrayList(list);
+		return list;
+	}
+
+	/**
 	 * Creates a new {@link ImmutableSet} while allowing you to add objects to it
 	 *
 	 * @param consumer Consumer used to add objects to the {@link ImmutableSet}
@@ -66,6 +79,21 @@ public final class Apex
 		if(set instanceof ImmutableSet)
 			return set;
 		return makeImmutableSet(builder -> builder.addAll(set));
+	}
+
+	/**
+	 * Converts specified {@link Set} into a Mutable set.
+	 *
+	 * @param set {@link Set} to be made mutable
+	 * @param <V> Object type of values inside the {@link Set}
+	 * @return The newly built set
+	 * @since 1.0.4-J8
+	 */
+	public static <V> Set<V> makeMutableSet(Set<V> set)
+	{
+		if(set instanceof ImmutableSet)
+			return Sets.newHashSet(set);
+		return set;
 	}
 
 	/**
@@ -96,5 +124,21 @@ public final class Apex
 		if(map instanceof ImmutableMap)
 			return map;
 		return makeImmutableMap(builder -> builder.putAll(map));
+	}
+
+	/**
+	 * Converts specified {@link Map} into a Mutable map.
+	 *
+	 * @param map {@link Map} to be made mutable
+	 * @param <K> Object type of keys inside the {@link Map}
+	 * @param <V> Object type of values inside the {@link Map}
+	 * @return The newly built map
+	 * @since 1.0.4-J8
+	 */
+	public static <K, V> Map<K, V> makeMutableList(Map<K, V> map)
+	{
+		if(map instanceof ImmutableMap)
+			return Maps.newHashMap(map);
+		return map;
 	}
 }
